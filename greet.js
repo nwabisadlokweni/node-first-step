73cafac9f5317ac9fe3cbb27df20b9eae1fa09c2
nwabisadlokweni <dlonwa001@gmail.com>
2020-08-04 11:06:53 +0200

    node changes

---
+++ greet.js
@@ -0,0 +1,9 @@
+module.exports = function(name){
+    return 'Hello, ' + name;
+}
+
+// function greet(name){
+//     var msg = greet('Nwabisa')
+//     console.log(msg)
+//     }
+  
---
+++ greet_nwabisa.js
@@ -0,0 +1,12 @@
+
+const chalk = require('chalk');
+//import the greet module that is in the current folder
+const greet = require('./greet');
+
+const styledMessage = chalk.bgGreen.black(greet('Nwabisa'));
+console.log(styledMessage)
+
+
+// const greet = require('./greet');
+
+// console.log(greet('Nwabisa'))
